f130c80fefe04a7452627f565c277de64d9ee7cb
Ankita <bobdeankita24@gmail.com>
2025-01-19 15:49:48 -0330

    crude operation

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
  const route = express.Router();
-// // const EmpController = require("../controllers/empController");
+const EmpController = require("../controllers/empController");
 
-// // route.get("/info", EmpController.empInfo);
+route.get("/info", EmpController.empInfo);
 route.post("/save", EmpController.empSave);
 route.get("/display", EmpController.empDisplay);
-// // route.post("/search", EmpController.empSearch);
-// // route.get("/searchbyquery", EmpController.empSearchByQuery);
-// // route.post("/savecreate", EmpController.empSaveCreate);
+route.post("/search", EmpController.empSearch);
+route.get("/searchbyquery", EmpController.empSearchByQuery);
+route.post("/savecreate", EmpController.empSaveCreate);
 
 
   module.exports = route;
